20b724712900b20ef0dcaad4fc0c4964b60e832a
Lufenny <lufennyapu@gmail.com>
2025-08-23 22:29:56 +0700

    Delete utils/helpers.py

--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import nltk
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-nltk.download('wordnet', quiet=True)
-def load_data(filepath='data/data.csv'):
-    return pd.read_csv(filepath) if os.path.exists(filepath) else None
