f8b0685166b4f4cf29736d89947ca7f126fa8c19
Lufenny <lufennyapu@gmail.com>
2025-08-25 22:36:15 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,23 +3,4 @@ st.set_page_config(page_title='Capstone Project Report', page_icon='📊', layou
 
 st.title("Capstone Project Report")
 
-# --- Step 1: Hypotheses ---
-st.header("🧩 Hypotheses / Expected Outcome")
-
-# --- Step 2: Data Sources ---
-st.header("📂 Data Sources")
-
-# --- Step 3: EDA ---
-st.header("🔍 Exploratory Data Analysis")
-    
-# --- Step 4: Data Process ---
-st.header("⚙️ Data Process")
-
-# --- Step 5: Modelling ---
-st.header("🤖 Modelling")
-
-# --- Step 6: Results ---
-st.header("📈 Results & Interpretation")
-    
-# --- Step 7: Deployment ---
-st.header("🚀 Deployment Details")
+st.header("Wealth Accumulation through Homeownership versus Renting and Investing: Evidence from Kuala Lumpur")
